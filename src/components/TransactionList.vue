5d07df6d49147753cbd69dc9092022a0d1d01ae3
TedEngineer <tedvu184@gmail.com>
2024-01-21 21:41:26 +1100

    start implementing list items

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,23 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
-      Cash <span>-$400</span><button class="delete-btn">x</button>
+    <li v-for="transaction in transactions" :key="transaction.id">
+      {{ transaction.text }} <span>-${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
     </li>
+
     <li class="plus">
       Paycheck <span>$800</span><button class="delete-btn">X</button>
     </li>
   </ul>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      transactions: [{ id: 1, text: 'Flower', amount: -19.99 }],
+    };
+  },
+};
+</script>
